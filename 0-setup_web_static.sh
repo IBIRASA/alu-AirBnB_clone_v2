1b6f1442ef82c13839c69ccefa5d8f81e0461a02
IBIRASA <d.ishimwe@alustudent.com>
2024-02-16 20:27:23 +0000

    all

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,22 @@
- #!/usr/bin/env bash
-# web static development
+#!/usr/bin/env bash
+# Install nginx and create a fake html file
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/index.html
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-sudo service nginx restart
+link_dir="/data/web_static/current"
+release_folder="/data/web_static/releases/test"
+HTML_CONTENT="<html>
+    <head>
+    </head>
+    <body>
+	My fake html file
+    </body>
+</html>"
+
+apt-get -y update >/dev/null 2>&1
+apt-get -y install nginx >/dev/null 2>&1
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "$HTML_CONTENT" >/data/web_static/releases/test/index.html
+ln -sf "$release_folder" "$link_dir"
+chown -R ubuntu:ubuntu /data/
+sed -i '/^server {/,/^}/!b;/^}/i\\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}' /etc/nginx/sites-enabled/default
+service nginx restart
