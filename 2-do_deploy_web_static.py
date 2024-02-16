d267521725b77a7339ae81d2fffb6c02e7807af5
IBIRASA <d.ishimwe@alustudent.com>
2024-02-16 20:41:08 +0000

    ip address

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.227.179.101', '3.90.204.71']
+env.hosts =['23.22.255.255', '54.167.93.232']
 
 
 def do_deploy(archive_path):
